0b9ff87c1892f5d1a1a0155a7b32d12e0eaf5159
Qomaa <>
2023-07-16 19:37:16 +0200

    handle sigterm, price format 0.00

--- source/Version.cs
+++ source/Version.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 internal  static class Version
 {
-    public const string VERSION = "0.0.2";
+    public const string VERSION = "0.0.3";
     public static string ProgramVersion =>  $"{Assembly.GetExecutingAssembly().GetName().Name} {VERSION}";
 }
